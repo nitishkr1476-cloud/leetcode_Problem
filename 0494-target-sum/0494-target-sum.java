deae4a9b5eb0c82889bae4852e46d3040c2a2ff1
nitishkr1476-cloud <nitishkr1476@gmail.com>
2026-04-11 22:00:24 +0530

    Time: 2 ms (99.96%), Space: 43.3 MB (53.27%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.java
@@ -0,0 +1,28 @@
+
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        int total = 0;
+        for (int num : nums) {
+            total += num;
+        }
+
+        // Edge cases
+        if ((total + target) % 2 != 0 || Math.abs(target) > total) {
+            return 0;
+        }
+
+        int subsetSum = (total + target) / 2;
+
+        // DP array
+        int[] dp = new int[subsetSum + 1];
+        dp[0] = 1;
+
+        for (int num : nums) {
+            for (int j = subsetSum; j >= num; j--) {
+                dp[j] += dp[j - num];
+            }
+        }
+
+        return dp[subsetSum];
+    }
+}
