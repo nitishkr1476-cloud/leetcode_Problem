8a98f78d8e784ffb3638b4b58aaafa2da59d74df
nitishkr1476-cloud <nitishkr1476@gmail.com>
2026-05-05 22:11:31 +0530

    Time: 15 ms (12.39%), Space: 47.5 MB (9.88%) - LeetHub

---
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.java
@@ -0,0 +1,60 @@
+
+import java.util.*;
+
+class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> result = new ArrayList<>();
+        
+        if (s == null || s.length() == 0 || words.length == 0) {
+            return result;
+        }
+
+        int wordLen = words[0].length();
+        int wordCount = words.length;
+        int totalLen = wordLen * wordCount;
+
+        if (s.length() < totalLen) return result;
+
+        // Frequency map of words
+        Map<String, Integer> wordMap = new HashMap<>();
+        for (String word : words) {
+            wordMap.put(word, wordMap.getOrDefault(word, 0) + 1);
+        }
+
+        // Try all possible starting offsets
+        for (int i = 0; i < wordLen; i++) {
+            int left = i, count = 0;
+            Map<String, Integer> windowMap = new HashMap<>();
+
+            for (int right = i; right + wordLen <= s.length(); right += wordLen) {
+                String word = s.substring(right, right + wordLen);
+
+                if (wordMap.containsKey(word)) {
+                    windowMap.put(word, windowMap.getOrDefault(word, 0) + 1);
+                    count++;
+
+                    // If more than expected, shrink window
+                    while (windowMap.get(word) > wordMap.get(word)) {
+                        String leftWord = s.substring(left, left + wordLen);
+                        windowMap.put(leftWord, windowMap.get(leftWord) - 1);
+                        left += wordLen;
+                        count--;
+                    }
+
+                    // If all words matched
+                    if (count == wordCount) {
+                        result.add(left);
+                    }
+
+                } else {
+                    // Reset window
+                    windowMap.clear();
+                    count = 0;
+                    left = right + wordLen;
+                }
+            }
+        }
+
+        return result;
+    }
+}
